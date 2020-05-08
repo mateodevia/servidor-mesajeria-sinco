36841c620df0deb01f4272da165b5e680d3244f8
mateodevia <m.devia@uniandes.edu.co>
2020-05-08 10:51:37 -0500

    Se configuró el servidor para que sirva de manera estatica el frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Sistema de Procesos Sincosoft</h1>
+        </div>
+    );
+}
+
+export default App;
